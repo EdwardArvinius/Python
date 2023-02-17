4cfe1d3820135ed361077ae185ac2a2bceabdbbc
Edward Arvinius <48127840+EdwardArvinius@users.noreply.github.com>
2023-02-17 17:27:16 +0100

    Update python_script.py

--- python_script/python_script.py
+++ python_script/python_script.py
@@ -1,71 +1,65 @@
 #!/usr/bin/python
 
+import os
+import shutil
+import fileinput
+import string
 
-#Edward Arvinius
-#CS 270
-
-
-
-ans=True
-while ans:
+while True:
     print("""
-    1.Add line number
-    2.Delete line number
-    3.Change file name extension
-    4.Backup file
-    5.Reverse file
-    6.Exit/Quit
+    1. Add line number
+    2. Delete line number
+    3. Change file name extension
+    4. Backup file
+    5. Reverse file
+    6. Exit/Quit
     """)
-    ans=raw_input("What would you like to do? ")
 
-    if ans=="1":
-      infile=open('line1.txt', 'r')
-      lines=infile.readlines()
-      infile.close()
-      outtext = ['%d %s' % (i, line) for i, line in enumerate(lines)]
-      outfile = open("line1.txt","w")
-      outfile.writelines(str("".join(outtext)))
-      outfile.close()
-      
-    elif ans=="2":
-      import fileinput
-      import string
+    choice = input("What would you like to do? ")
 
-      for line in fileinput.input("line1.txt", inplace=True):
-          print line.translate(None, string.digits),
+    if choice == "1":
+        infile = open('line1.txt', 'r')
+        lines = infile.readlines()
+        infile.close()
+        outtext = ['%d %s' % (i, line) for i, line in enumerate(lines)]
+        outfile = open("line1.txt", "w")
+        outfile.writelines(str("".join(outtext)))
+        outfile.close()
 
-    elif ans=="3":
-      print("\nOld file name:")
-      print('file.cxx')
-      print("\nNew file name:")
-      import os
-      print os.path.splitext('file.cxx')[0]+'.cpp'
-	
-    elif ans=="4":
+    elif choice == "2":
+        for line in fileinput.input("line1.txt", inplace=True):
+            print(line.translate(None, string.digits), end='')
 
-#     
-#	import os
-	import shutil
-#        backup = ".backup"
-#    if not os.path.exists(backup):
-#        os.makedirs(backup)
-#
-#    if os.path.exists(backup):
-#        file_path = dir + file
-#        
-#        # move files into created directory
-#        shutil.move(file_path, backup)
+    elif choice == "3":
+        print("\nOld file name: file.cxx")
+        print("\nNew file name: " + os.path.splitext('file.cxx')[0] + '.cpp')
 
-    elif ans=="5":
-	print("\nBefore reverse")
-	f = open('text1.txt', 'r')
-	file_contents = f.read()
-	print (file_contents)		
-	print ("After reverse")		
-	for line in reversed(open("text1.txt").readlines()):
-    		print line.rstrip()
-    elif ans=="6":
-      print("\n Goodbye!") 
-      ans = None
+    elif choice == "4":
+        file_path = 'line1.txt'
+        backup_dir = '.backup'
+
+        if not os.path.exists(backup_dir):
+            os.makedirs(backup_dir)
+
+        # Copy the file to the backup directory
+        shutil.copy2(file_path, os.path.join(backup_dir, os.path.basename(file_path) + '.bak'))
+        print(f'File {file_path} backed up successfully.')
+
+    elif choice == "5":
+        print("\nBefore reverse")
+        with open('text1.txt', 'r') as f:
+            file_contents = f.read()
+            print(file_contents)
+
+        print("\nAfter reverse")
+        with open('text1.txt', 'r') as f:
+            for line in reversed(f.readlines()):
+                print(line.rstrip())
+
+    elif choice == "6":
+        print("\nGoodbye!")
+        break
+
     else:
-       print("\n Not Valid Choice Try again")
+        print("\nNot a valid choice. Try again.")
+
